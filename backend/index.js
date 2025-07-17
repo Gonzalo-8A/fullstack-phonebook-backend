290f716fbaf6078af2aa1e34e00214ba7293e416
Gonzalo <gonzalo8amartin@gmail.com>
2025-07-17 15:25:05 +0200

    Exercises 3.15-3.18

--- backend/index.js
+++ backend/index.js
@@ -29,26 +29,25 @@ app.get('/api/info', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.find({}).then((persons) => {
-    const id = request.params.id
-    const person = persons.find((p) => p.id === id)
-
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(() => {
-    response.status(204).end()
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => response.status(204).end())
+    .catch((error) => next(error))
 })
 
-app.post('/api/persons', async (request, response) => {
+app.post('/api/persons', async (request, response, next) => {
   const { name, number } = request.body
 
   if (!name) {
@@ -57,6 +56,7 @@ app.post('/api/persons', async (request, response) => {
   if (!number) {
     return response.status(400).json({ error: 'number missing' })
   }
+
   try {
     const existingPerson = await Person.findOne({ name })
     if (existingPerson) {
@@ -64,18 +64,59 @@ app.post('/api/persons', async (request, response) => {
     }
 
     const person = new Person({ name, number })
-
     const savedPerson = await person.save()
-
     response.json(savedPerson)
   } catch (error) {
-    console.log(error)
+    next(error)
+  }
+})
+
+app.put('/api/persons/:id', async (request, response, next) => {
+  const { name, number } = request.body
+
+  const updatedPerson = {
+    name,
+    number,
+  }
+
+  try {
+    const result = await Person.findByIdAndUpdate(request.params.id, updatedPerson, {
+      new: true,
+      runValidators: true,
+      context: 'query',
+    })
+    if (result) {
+      response.json(result)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
   }
 })
 
 app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
