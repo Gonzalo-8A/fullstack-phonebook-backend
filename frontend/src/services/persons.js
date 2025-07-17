507be2ee5b5bed39f37c751466187220e991d9bd
Gonzalo <gonzalo8amartin@gmail.com>
2025-07-17 14:31:32 +0200

    update URL

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:3001/api/persons'
+const baseUrl = 'https://fullstack-phonebook-backend-zu29.onrender.com/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
